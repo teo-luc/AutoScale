b2c65b0ee639c9832791c77252e830105c48a1b9
William Nguyen <william@forixwebdesign.com>
2019-12-01 15:09:43 +0700

    init src

---
+++ AutoScale/AutoScale/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoScale
+//
+//  Created by Tèo Lực on 12/1/19.
+//  Copyright © 2019 Luc Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
